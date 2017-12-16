d70c49112468be676faa651875190954f7297ac3
MigLexVic <miguel@brutus-dev.com>
2017-12-16 22:47:58 +0000

    L

--- index.js
+++ index.js
@@ -1,3 +1,1 @@
-/**
- * Created by capes on 16/12/2017.
- */
+module.exports = require('./src/RuntimeJsCache')
--- src/RuntimeJsCache.js
+++ src/RuntimeJsCache.js
@@ -1,3 +1,47 @@
-/**
- * Created by capes on 16/12/2017.
- */
+function insertInCache (key, value) {
+    CONFIG.storage.setItem( 'RJC_info_' + key, JSON.stringify({created: date.now(), expires: (date.now() + CONFIG.expires), forceUpdate: false}))
+    CONFIG.storage.setItem( 'RJC_data_' + key, JSON.stringify(value) )
+}
+
+function getFromCache (key) {
+    var info = JSON.parse(CONFIG.storage.getItem('RJC_info_' + key))
+    if(info === null || info === undefined || info.expires >= date.now()){
+        CONFIG.storage.removeItem('RJC_info_' + key)
+        CONFIG.storage.removeItem('RJC_data_' + key)
+        return 'Cache Expired'
+    }
+
+    var data = JSON.parse(CONFIG.storage.getItem('RJC_data_' + key))
+    if(data === null || data === undefined) return 'Invalid Content'
+
+    return data
+}
+
+function clearFromCache (key) {
+    // if(key === null) console.log('will clear all the cache items, then return')
+    CONFIG.storage.removeItem('RJC_info_' + key)
+    CONFIG.storage.removeItem('RJC_data_' + key)
+}
+function forceUpdateCache (key) {
+    console.log('Force Cache element update', key)
+}
+
+var CONFIG = {
+    storage: localStorage,
+    expires: 60000
+}
+
+var setInstance = function () {
+    return {
+        Config: function (prop, val) { CONFIG[prop] = val},
+        set: insertInCache,
+        get: getFromCache,
+        clear: clearFromCache,
+        forceReset: forceUpdateCache
+    }
+}
+
+var RJC = setInstance()
+
+module.exports = RJC;
+module.exports.default = RJC;
